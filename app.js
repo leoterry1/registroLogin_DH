eaf547fe07d230acd5b442ed38b4099f6c687f0c
Leo Terry <lucasenriquez1997@gmail.com>
2021-01-29 16:06:38 -0300

    terminado

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const usersRouter = require("./routes/users")
 
 
-app.use("/user", usersRouter)
+
 
 app.set('view engine','ejs'); 
 app.set('views',__dirname + '/views');
@@ -12,5 +12,5 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-
+app.use("/user", usersRouter)
 app.listen(3030, ()=> console.log("El servidor está corriendo en el puerto 3030"))
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,58 +1,68 @@
 let bcrypt = require("bcrypt")
 const fs = require("fs");
 module.exports = {
-    formLogin: (req,res)=>{
+    formLogin: (req, res) => {
         console.log(req.body)
         let mensaje = false
-        res.render("login", {mensaje})
+        res.render("login", { mensaje })
     },
-    login: (req,res)=>{
-        let {email, password} = req.body
+    login: (req, res) => {
+        let { email, password } = req.body
         let archivoUsuarios = fs.readFileSync("./public/data/usuarios.json", "utf-8")
         let usuarios = JSON.parse(archivoUsuarios)
-        let usuario = usuarios.find(usuario =>{
+        let usuario = usuarios.find(usuario => {
             return usuario.email === email
         })
-        if(usuario == undefined){
-            res.render("login", {mensaje: "El usuario no se encuentra registrado"})
-        } else if (bcrypt.compareSync(usuario.password, password)){
-            res.render("profile", {usuario})
+        if (req.body == undefined || password == "" || email == "") {
+            res.render("login", { mensaje: "Por favor, ingrese los datos requeridos" })
         } else {
-            res.render("login", {mensaje:"Contraseña incorrecta"})
+            if (usuario == undefined) {
+                res.render("login", { mensaje: "El usuario no se encuentra registrado" })
+            } else if (bcrypt.compareSync(password, usuario.password)) {
+                res.render("profile", { usuario })
+            } else {
+                res.render("login", { mensaje: "Contraseña incorrecta" })
+            }
         }
 
+
     },
-    formRegister: (req,res)=>{
-        let error = false
-        res.render("register",{error})
+    formRegister: (req, res) => {
+        let asterisco = false
+        let mensaje = false
+        res.render("register", { asterisco, mensaje })
     },
-    register: (req,res, next)=>{
-        let {name , email , password} = req.body
+    register: (req, res, next) => {
+        let { name, email, password } = req.body
         let archivoUsuarios = fs.readFileSync("./public/data/usuarios.json", "utf-8")
         let usuarios = JSON.parse(archivoUsuarios)
-        let existe = usuarios.find(usuario =>{
+        let img
+        if (req.files[0] == undefined) {
+            img = "perfil.jpg"
+        } else {
+            img = req.files[0].filename
+        }
+        let existe = usuarios.find(usuario => {
             return usuario.email === email
         })
-            if(existe === undefined){
-                console.log(existe)
+        if (req.body == undefined || name == "" || email == "" || password == "") {
+            res.render("register", { mensaje: "Por favor, complete todos los campos", asterisco: "*" })
+        } else {
+            if (existe === undefined) {
                 let usuario = {
                     name,
                     email,
                     password: bcrypt.hashSync(password, 10),
-                    img : req.files[0].filename
+                    img
                 }
-                
-                usuarios.push(usuario)
-                fs.writeFileSync("./public/data/usuarios.json",(JSON.stringify(usuarios)))
-                res.redirect("/user/login")
-                
-            }else{
-                error = true
-                res.render("register", {error})
-            }
-            
-            
-        
 
+                usuarios.push(usuario)
+                fs.writeFileSync("./public/data/usuarios.json", (JSON.stringify(usuarios)))
+                res.redirect("/user/login")
+
+            }else {
+                res.render("register", { mensaje: `El correo electrónico ya se encuentra registrado, si es tu cuenta, <a href="/user/login">inicia sesión.</a>`, asterisco: false })
+            }
+        }
     }
 }
